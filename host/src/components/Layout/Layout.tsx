ae35a2223f6b788938a79171578046f226913a6e
gizeasy <gizeasy@gmail.com>
2021-08-10 14:57:55 +0200

    chore(tests): added deps and examples

--- host/src/components/Layout/Layout.tsx
+++ host/src/components/Layout/Layout.tsx
@@ -1,21 +1,22 @@
-import React, {
-    FunctionComponent,
-    ReactNode
-} from 'react';
+import React, { FunctionComponent, ReactNode } from 'react';
 
 import styles from './Layout.module.css';
 
 type Props = {
-    header: ReactNode;
+  header: ReactNode;
+} & JSX.IntrinsicElements['div'];
+
+const Layout: FunctionComponent<Props> = ({
+  children,
+  header,
+  ...otherProps
+}) => {
+  return (
+    <div {...otherProps} className={styles.layout}>
+      <header>{header}</header>
+      <main className={styles.content}>{children}</main>
+    </div>
+  );
 };
 
-const Layout: FunctionComponent<Props> = ({children, header}) => {
-    return (
-        <div className={styles.layout}>
-            <header>{header}</header>
-            <main className={styles.content}>{children}</main>
-        </div>
-    );
-};
-
-export {Layout};
+export { Layout };
