43926e772b8d53b6349a366b72580d43a2c41169
Alexander Abashkin <monolithed@gmail.com>
2021-08-11 10:42:22 +0300

    Fixed config

--- tutorial-stream/__mocks__/proxyApi.js
+++ tutorial-stream/__mocks__/proxyApi.js
@@ -1,5 +1,11 @@
 const proxy = {
-
+    _proxy: {
+        header: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+        }
+    }
 };
 
 module.exports = proxy;
